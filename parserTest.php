fd4fccaa791df5eb82a8d0d3fe549c62d240fad0
Perry van Wesel <perry.wesel@gmail.com>
2017-06-16 17:33:02 +0200

    Support for quoteless strings

--- parserTest.php
+++ parserTest.php
@@ -4,6 +4,7 @@
 	$parser = new \ValveKV\ValveKV();
 	//print_r($parser->parseFromString(file_get_contents("TestData/conditional.kv")));
 	//print_r($parser->parseFromString(file_get_contents("TestData/quoteless.kv")));
-	print_r($parser->parseFromString(file_get_contents("TestData/dota_english.txt")));
+	print_r($parser->parseFromString(file_get_contents("TestData/quoteless2.kv")));
+	//print_r($parser->parseFromString(file_get_contents("TestData/dota_english.txt")));
 
 	//var_dump($parser->parseFromString('"HELLO" { "A" " B " "C" "DE"}'));
