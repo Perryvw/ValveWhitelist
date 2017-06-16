8d5de0386d5231b6b0102bfae9fca60ca928f4c9
Perry van Wesel <perry.wesel@gmail.com>
2017-06-16 16:48:08 +0200

    Encoding handling, conditionals, better parseString

---
+++ parserTest.php
@@ -0,0 +1,9 @@
+<?php
+	require "valveKV.php";
+
+	$parser = new \ValveKV\ValveKV();
+	//print_r($parser->parseFromString(file_get_contents("TestData/conditional.kv")));
+	//print_r($parser->parseFromString(file_get_contents("TestData/quoteless.kv")));
+	print_r($parser->parseFromString(file_get_contents("TestData/dota_english.txt")));
+
+	//var_dump($parser->parseFromString('"HELLO" { "A" " B " "C" "DE"}'));
